8a313a40719e7668691c94d638724598edb12c31
Keith McDonald <keith.mcdonald@salesforce.com>
2018-05-28 18:28:35 -0700

    Add ability to generate collages of four, rather than five, images.

--- week_in_review.sh
+++ week_in_review.sh
@@ -2,12 +2,20 @@
 
 # Scale and crop all images
 
-function scale_and_crop {
+function scale_and_crop_square {
   for filename in *.JPG; do
     convert "${filename}" -resize 1080x1080 1080_resize_"${filename}.PNG"
     smartcrop --width 1080 --height 1080 1080_resize_"${filename}.PNG" 1080_crop_"${filename}.PNG"
     rm 1080_resize_"${filename}.PNG"
     convert 1080_crop_"${filename}.PNG" -resize 540x540 540_crop_"${filename}.PNG"
+  done
+}
+
+function scale_and_crop_wide {
+  for filename in *.JPG; do
+    convert "${filename}" -resize 1080x540 1080_resize_"${filename}.PNG"
+    smartcrop --width 1080 --height 540 1080_resize_"${filename}.PNG" 1080_crop_"${filename}.PNG"
+    rm 1080_resize_"${filename}.PNG"
   done
 }
 
@@ -31,7 +39,7 @@ perm() {
 }
 
 # Create all 120 permutations of 5 images
-function create_permutations {
+function create_permutations_of_5 {
   ARRAY=()
 
   i=1
@@ -71,11 +79,58 @@ function create_permutations {
   done
 }
 
-# Scale/crop images to 540x540 and 1080x1080
-scale_and_crop
+function create_permutations_of_4 {
+  ARRAY=()
+
+  i=1
+  for filename in 1080_crop_*.PNG; do
+    files_1080[i]="${filename}"
+    ((i+=1))
+  done
+
+  perm "1234"
+  for permutation in ${ARRAY[*]}; do
+    if [ ! -r "vert${permutation:0:1}${permutation:1:1}.PNG" ]; then
+      convert ${files_1080[${permutation:0:1}]} \
+              ${files_1080[${permutation:1:1}]} \
+              -append "vert${permutation:0:1}${permutation:1:1}.PNG"
+    fi
+    if [ ! -r "vert${permutation:2:1}${permutation:3:1}.PNG" ]; then
+      convert ${files_1080[${permutation:2:1}]} \
+              ${files_1080[${permutation:3:1}]} \
+              -append "vert${permutation:2:1}${permutation:3:1}.PNG"
+    fi
+    convert "vert${permutation:0:1}${permutation:1:1}.PNG" \
+            "vert${permutation:2:1}${permutation:3:1}.PNG" \
+            +append WeekInReview${permutation}.JPG
+  done
+
+  for permutation in ${ARRAY[*]}; do
+    if [ -r "vert${permutation:0:1}${permutation:1:1}.PNG" ]; then
+      rm "vert${permutation:0:1}${permutation:1:1}.PNG"
+    fi
+  done
+}
 
 # Create all 120 permutations of the 5 square images
-create_permutations
+count=`ls -1 *.JPG 2>/dev/null | wc -l`
+if [ $count != 5 ] && [ $count != 4 ]; then
+  echo "Expected to find 4 or 5 files with extension JPG and found ${count} files."
+  exit
+else 
+  if [ $count == 5 ]; then
+    # Scale/crop images to 540x540 and 1080x1080
+    scale_and_crop_square
 
-# Clean up the temporary files
-rm_scaled_and_cropped
+    create_permutations_of_5
+  elif [ $count == 4 ]; then
+    # Scale/crop images to 1080x540
+    scale_and_crop_wide
+
+    create_permutations_of_4
+  fi
+
+  # Clean up the temporary files
+  rm_scaled_and_cropped
+fi
+
